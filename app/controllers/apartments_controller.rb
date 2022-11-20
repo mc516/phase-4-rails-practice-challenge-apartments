3e60bd3c911fa6fb75b4a80090206cb0e70e6af3
Mickey <mickeyc11111@gmail.com>
2022-11-20 12:03:14 -0500

    CRUD for apartments

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -2,4 +2,46 @@ class ApartmentsController < ApplicationController
     def index
         render json: Apartment.all
     end
+
+    def show
+        apartment = Apartment.find(params[:id])
+        if apartment
+            render json: apartment
+        else
+            render json: { error: "Apartment not found" }, status: :not_found
+        end
+    end
+
+    def create
+        apartment = Apartment.create(number: params[:number])
+        if apartment.valid?
+            render json: apartment, status: :created
+        else
+            render json: { error: apartment.errors },status: :not_created
+        end
+    end
+
+    def update
+        apartment = Apartment.find_by(id: params[:id])
+        if apartment.valid?
+            apartment.update(number:params[:number])
+            render json: apartment
+        else
+            render json: { error: apartment.errors },status: :not_found
+        end
+      
+    end
+
+    def destroy
+        apartment = Apartment.find_by(id: params[:id])
+        if apartment
+            apartment.destroy
+            head :no_content
+        else
+            render json: { error: "Apartment not found"}, status: :not_found
+        end
+
+    end
+
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tenants, only: [:index, :show, :update, :create, :delete]
-  resources :apartments, only: [:index, :show, :update, :create, :delete]
+  resources :tenants, only: [:index, :show, :update, :create, :destroy]
+  resources :apartments, only: [:index, :show, :update, :create, :destroy]
   resources :leases, only: [:create, :delete]
 
 end
